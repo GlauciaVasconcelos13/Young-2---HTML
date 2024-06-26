374cc363df10866962d34fe208a51e95348511d2
Glauciavas123 <glauciagsmv@gmail.com>
2024-06-25 21:29:56 -0300

    Trabalho final - finalizando...

---
+++ Trabalho Final/script.js
@@ -0,0 +1,19 @@
+const FOTO = document.querySelector ('.foto')
+const GITHUB = document.querySelector ('.github')
+const INSTAGRAM = document.querySelector ('.instagram')
+const EMAIL = document.querySelector ('.email')
+const TITULO= document.querySelector ('.titulo')
+const TEXTO = document.querySelector ('.texto')
+const BOTAO = document.querySelector ('.botao')
+
+botao.addEventListener ('click', salvar_dados)
+
+function salvar_dados () {
+    localStorage.setItem ('FOTO', FOTO.value)
+    localStorage.setItem ('GITHUB', GITHUB.value)
+    localStorage.setItem ('INSTAGRAM', INSTAGRAM.value)
+    localStorage.setItem ('EMAIL', EMAIL.value)
+    localStorage.setItem ('TITULO', TITULO.value)
+    localStorage.setItem ('TEXTO', TEXTO.value)
+}
+
